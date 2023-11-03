b8507e9ed79a4f9da107ee20b393229130453624
Asim Zulfiqar <32940052+AsimZulfiqar67@users.noreply.github.com>
2023-11-03 16:24:03 +0100

    Update livingroom.ino

--- chapter 8/livingroom/livingroom.ino
+++ chapter 8/livingroom/livingroom.ino
@@ -3,6 +3,8 @@
 #include <WiFiMulti.h>
 #include <InfluxDbClient.h> //https://github.com/tobiasschuerg/InfluxDB-Client-for-Arduino.git
 #include <InfluxDbCloud.h>
+#include <PubSubClient.h>
+#include <ESP32Servo.h>
 
 // WiFi configuration
 WiFiMulti wifiMulti;
@@ -28,8 +30,20 @@ const int motionsensor = 14;
 DHT_Unified dht(DHTPIN, DHTTYPE);
 uint32_t delayMS;
 
+// Servo setup
+Servo doorLock;
+int servoPosition = 90; // Initial position
+
+// MQTT settings
+const char *mqttServer = "broker.hivemq.com";
+const char *clientID = "client_ID";
+const char *topic = "/door/lock";
+
+WiFiClient espClient;
+PubSubClient mqttclient(espClient);
+
 // Time zone information
-const char* TZ_INFO = "UTC1";
+const char *TZ_INFO = "UTC1";
 
 // InfluxDB client instance
 InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN, InfluxDbCloud2CACert);
@@ -37,44 +51,55 @@ InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKE
 // Data point
 Point dbdata("House_data");
 
-void setupWifi() {
+void setupWifi()
+{
   WiFi.mode(WIFI_STA);
   wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
-  
+
   Serial.print("Connecting to Wi-Fi");
-  while (wifiMulti.run() != WL_CONNECTED) {
+  while (wifiMulti.run() != WL_CONNECTED)
+  {
     Serial.print(".");
     delay(100);
   }
   Serial.println();
 }
 
-void setupSensors() {
+void setupSensors()
+{
   dht.begin();
   sensor_t sensor;
   dht.temperature().getSensor(&sensor);
   dht.humidity().getSensor(&sensor);
-  
+
   pinMode(LDR, INPUT);
   pinMode(motionsensor, INPUT);
-  
+
   // Sync time
   timeSync(TZ_INFO, "pool.ntp.org", "time.nis.gov");
-  
-  if (client.validateConnection()) {
+
+  if (client.validateConnection())
+  {
     Serial.print("Connected to InfluxDB: ");
     Serial.println(client.getServerUrl());
-  } else {
+  }
+  else
+  {
     Serial.print("InfluxDB connection failed: ");
     Serial.println(client.getLastErrorMessage());
   }
   dbdata.addTag("device", DEVICE);
   dbdata.addTag("SSID", WiFi.SSID());
+
+    // Servo setup
+  doorLock.attach(15); // Attach the servo to pin D15
+  doorLock.write(servoPosition); // Set initial position
 }
 
-void readSensors() {
+void readSensors()
+{
   dbdata.clearFields();
-  
+
   sensors_event_t event;
   dht.temperature().getEvent(&event);
   dbdata.addField("temperature", event.temperature);
@@ -85,28 +110,99 @@ void readSensors() {
   dbdata.addField("light", digitalRead(LDR));
 }
 
-void writeToInfluxDB() {
+void writeToInfluxDB()
+{
   Serial.print("Writing: ");
   Serial.println(dbdata.toLineProtocol());
 
-  if (!client.writePoint(dbdata)) {
+  if (!client.writePoint(dbdata))
+  {
     Serial.print("InfluxDB write failed: ");
     Serial.println(client.getLastErrorMessage());
   }
 
   Serial.println("Waiting 1 second");
-  delay(5000);
+  
 }
 
-void setup() {
+void callback(char *topic, byte *payload, unsigned int length)
+{
+   Serial.print("Received message on topic: ");
+  Serial.println(topic);
+
+  Serial.print("Message payload: ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+  if (strcmp(topic, "/door/lock") == 0)
+  {
+    if (payload[0] == '1')
+    {
+      // Open the door lock
+      doorLock.write(0); // Adjust the servo position for the open state
+      Serial.println("door opened");
+      delay(500);       // Keep it open for 5 seconds
+      // Close the door lock
+      doorLock.write(servoPosition); // Return to the initial position
+    }
+  }
+}
+
+void reconnect()
+{
+  while (!mqttclient.connected())
+  {
+    if (mqttclient.connect(clientID))
+    {
+      mqttclient.subscribe(topic);
+    }
+    else
+    {
+      delay(5000);
+    }
+  }
+}
+
+void mqttsetup()
+{
+  // MQTT setup
+  mqttclient.setServer(mqttServer, 1883);
+  mqttclient.setCallback(callback);
+  mqttclient.setKeepAlive(15);
+
+  if (mqttclient.connect(clientID))
+  {
+    mqttclient.subscribe(topic);
+  }
+}
+
+void mqttTask(void *pvParameters) {
+  // MQTT setup code here
+
+  while (1) {
+    // MQTT loop code here
+    if (!mqttclient.connected()) {
+      // Reconnect to MQTT broker
+     reconnect();
+    }
+    mqttclient.loop();
+    // Add any other MQTT-related logic here
+  }
+}
+
+void setup()
+{
   Serial.begin(115200);
   setupWifi();
   setupSensors();
+  mqttsetup();
+  xTaskCreatePinnedToCore(mqttTask, "MQTT Task", 8192, NULL, 1, NULL, 0);
+
 }
 
-void loop() {
+void loop()
+{
   readSensors();
   writeToInfluxDB();
-delay(1000);
 }
-
